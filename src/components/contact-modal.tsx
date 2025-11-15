886c3214f6af437d0798364bc443e2c594ac27d7
DimDimich-creator <stilys62@gmail.com>
2025-11-15 16:36:24 +0300

    feat: add contact modal form

---
+++ src/components/contact-modal.tsx
@@ -0,0 +1,26 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import ContactForm from "./contact-form";
+
+export default function ContactModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <ContactForm />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
