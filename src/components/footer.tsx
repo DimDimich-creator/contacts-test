32283d52d1cdae6c565ca618793857866464d379
DimDimich-creator <stilys62@gmail.com>
2025-11-17 00:48:53 +0300

    feat: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Nav } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer className="py-3 mt-4 border-top">
+      <Container className="d-flex flex-column flex-sm-row justify-content-between align-items-center">
+        <span className="text-muted">
+          {new Date().getFullYear()} ♡ Dmitry Kashirin
+        </span>
+      </Container>
+    </footer>
+  );
+}
