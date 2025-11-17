5a506dd10c0cef82d854189e32218a43de00dca1
DimDimich-creator <stilys62@gmail.com>
2025-11-17 04:13:28 +0300

    feat: upd theme to localstorage

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -4,40 +4,57 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Dropdown } from "react-bootstrap";
 
+const THEME_KEY = "theme";
+
 export function ModeToggleBootstrap() {
   const [mounted, setMounted] = useState(false);
   const [theme, setTheme] = useState<"light" | "dark" | "system">("light");
 
-  useEffect(() => {
-    setMounted(true);
-    const current = document.documentElement.getAttribute("data-bs-theme") as
-      | "light"
-      | "dark"
-      | null;
-
-    if (current) setTheme(current);
-  }, []);
-
-  if (!mounted) return null;
-
+  // declare function before useEffect
   const applyTheme = (value: "light" | "dark" | "system") => {
+    let applied = value;
+
     if (value === "system") {
       const prefersDark = window.matchMedia(
         "(prefers-color-scheme: dark)",
       ).matches;
-      value = prefersDark ? "dark" : "light";
+      applied = prefersDark ? "dark" : "light";
     }
+
+    localStorage.setItem(THEME_KEY, value);
 
     if (document.startViewTransition) {
       document.startViewTransition(() => {
-        document.documentElement.setAttribute("data-bs-theme", value);
+        document.documentElement.setAttribute("data-bs-theme", applied);
         setTheme(value);
       });
     } else {
-      document.documentElement.setAttribute("data-bs-theme", value);
+      document.documentElement.setAttribute("data-bs-theme", applied);
       setTheme(value);
     }
   };
+
+  useEffect(() => {
+    setMounted(true);
+
+    const savedTheme = localStorage.getItem(THEME_KEY) as
+      | "light"
+      | "dark"
+      | "system"
+      | null;
+
+    if (savedTheme) {
+      applyTheme(savedTheme);
+    } else {
+      const current = document.documentElement.getAttribute("data-bs-theme") as
+        | "light"
+        | "dark"
+        | null;
+      if (current) setTheme(current);
+    }
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <Dropdown onSelect={(key) => applyTheme(key as any)}>
@@ -56,7 +73,9 @@ export function ModeToggleBootstrap() {
           Dark
         </Dropdown.Item>
 
-        <Dropdown.Item eventKey="system">System</Dropdown.Item>
+        <Dropdown.Item eventKey="system" active={theme === "system"}>
+          System
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
